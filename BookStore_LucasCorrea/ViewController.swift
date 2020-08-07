46f67a35ba735152bb962ba93888b04271a7ef5f
Lucas Correa <contato@lucascorrea.com>
2020-08-06 21:46:01 -0300

    Initial Commit

---
+++ BookStore_LucasCorrea/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookStore_LucasCorrea
+//
+//  Created by Lucas Correa on 06/08/2020.
+//  Copyright © 2020 SiriusCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookStore_LucasCorreaTests/BookStore_LucasCorreaTests.swift
@@ -0,0 +1,34 @@
+//
+//  BookStore_LucasCorreaTests.swift
+//  BookStore_LucasCorreaTests
+//
+//  Created by Lucas Correa on 06/08/2020.
+//  Copyright © 2020 SiriusCode. All rights reserved.
+//
+
+import XCTest
+@testable import BookStore_LucasCorrea
+
+class BookStore_LucasCorreaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
